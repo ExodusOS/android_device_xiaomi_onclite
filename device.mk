12a1aed220e056ed542cbdfdd62d9b2029e9e1b8
Pranav Vashi <neobuddy89@gmail.com>
2022-12-26 19:13:30 +0300

    exodus:Enable smart charging * exodus feature
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -395,7 +395,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.smartcharging.rc
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
